9bddb451a609169a58871cb7450118a9a8f8741d
Phil96 <PhilipGnann96@aol.com>
2014-09-13 23:08:37 +0200

    Added statistic calculations

--- src/Calculatings/Calculator.java
+++ src/Calculatings/Calculator.java
@@ -1,7 +1,49 @@
 package Calculatings;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+import parser.Parser;
+
 public class Calculator {
 	
 	private Classroom classroom;
+	
+	
+	private Parser p = new Parser();
+			String inputFile = "C:\\Users\\getränke-gnann1911\\Downloads\\data.csv";
+	public Classroom fillClass(){
+		HashMap<String, Integer> cls = p.parse(inputFile);
+		
+		//Daten aus der HashMap holen
+		int foreigners = cls.get("Schüler ausländisch");
+		int total = cls.get("Schüler insgesamt");
+		int female = cls.get("Schüler weiblich");
+		int male = cls.get("Schüler männlich");
+		int teacher = 93103; // Lehrerzahl wurde unabhängig von oben genutzter Statistik recherchiert
+		
+		//Berechnung des Klassenzimmers
+		double percentageGirls = female/ (double)total;
+		double percentageBoys = male/ (double)total;
+		double percentageForeigners = foreigners/ (double)total;
+		int studentsPerTeacher = (int) Math.round(total/(double)teacher);
+		int boysInClass = (int) Math.round(percentageBoys*(double)studentsPerTeacher);
+		int girlsInClass = studentsPerTeacher - boysInClass;
+		int foreignersInClass = (int)Math.round(percentageForeigners*(double)studentsPerTeacher);
+		
+		//System.out.print();	// debugging
+		
+		
+		//Classroom avgClass = new Classroom();
+		return null;
+	}
+	
+	public static void main(String[] args)
+	{
+		new Calculator().fillClass();
+	}
 
 }
--- src/Calculatings/Classroom.java
+++ src/Calculatings/Classroom.java
@@ -2,20 +2,28 @@ package Calculatings;
 
 public class Classroom {
 	
-	private double studentsPerTeacher;
+	private int studentsPerTeacher;
 	private double percentageOfGirls;
+	private double percentageOfBoys;
 	private double percentageOfForeigners;
+	private int girlsInClass;
+	private int boysInClass;
+	private int foreignersInClass;
 	
-	public Classroom(double studentsPerTeacher, double percentageOfGirls, double percentageOfForeigners){
+	public Classroom(int studentsPerTeacher, double percentageOfGirls, double percentageOfBoys, double percentageOfForeigners, int girlsInClass, int boysInClass, int foreignersInClass){
 		this.studentsPerTeacher  = studentsPerTeacher;
 		this.percentageOfGirls = percentageOfGirls;
+		this.setPercentageOfBoys(percentageOfBoys);
 		this.percentageOfForeigners = percentageOfForeigners;
+		this.setGirlsInClass(girlsInClass);
+		this.setBoysInClass(boysInClass);
+		this.setForeignersInClass(foreignersInClass);
 	}
 	
 	public double getStudentsPerTeacher() {
 		return studentsPerTeacher;
 	}
-	public void setStudentsPerTeacher(double studentsPerTeacher) {
+	public void setStudentsPerTeacher(int studentsPerTeacher) {
 		this.studentsPerTeacher = studentsPerTeacher;
 	}
 	public double getPercentageOfGirls() {
@@ -29,6 +37,38 @@ public class Classroom {
 	}
 	public void setPercentageOfForeigners(double percentageOfForeigners) {
 		this.percentageOfForeigners = percentageOfForeigners;
+	}
+
+	public double getPercentageOfBoys() {
+		return percentageOfBoys;
+	}
+
+	public void setPercentageOfBoys(double percentageOfBoys) {
+		this.percentageOfBoys = percentageOfBoys;
+	}
+
+	public int getGirlsInClass() {
+		return girlsInClass;
+	}
+
+	public void setGirlsInClass(int girlsInClass) {
+		this.girlsInClass = girlsInClass;
+	}
+
+	public int getBoysInClass() {
+		return boysInClass;
+	}
+
+	public void setBoysInClass(int boysInClass) {
+		this.boysInClass = boysInClass;
+	}
+
+	public int getForeignersInClass() {
+		return foreignersInClass;
+	}
+
+	public void setForeignersInClass(int foreignersInClass) {
+		this.foreignersInClass = foreignersInClass;
 	}
 	
 

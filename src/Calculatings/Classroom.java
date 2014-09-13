d0669b0bfc222728a7374d018894ac48390f9021
senf <senf.faller@yahoo.de>
2014-09-13 12:32:53 +0200

    An Object of this class will be given to the UI. It should contain all requiered information. It will be initalized from the calculator class.

--- src/Calculatings/Classroom.java
+++ src/Calculatings/Classroom.java
@@ -2,9 +2,34 @@ package Calculatings;
 
 public class Classroom {
 	
-//	private int numberOfStudents;
-	private int numberOfTeachers;
-	private double StudentsPerTeacher;
-	private int number
+	private double studentsPerTeacher;
+	private double percentageOfGirls;
+	private double percentageOfForeigners;
+	
+	public Classroom(double studentsPerTeacher, double percentageOfGirls, double percentageOfForeigners){
+		this.studentsPerTeacher  = studentsPerTeacher;
+		this.percentageOfGirls = percentageOfGirls;
+		this.percentageOfForeigners = percentageOfForeigners;
+	}
+	
+	public double getStudentsPerTeacher() {
+		return studentsPerTeacher;
+	}
+	public void setStudentsPerTeacher(double studentsPerTeacher) {
+		this.studentsPerTeacher = studentsPerTeacher;
+	}
+	public double getPercentageOfGirls() {
+		return percentageOfGirls;
+	}
+	public void setPercentageOfGirls(double percentageOfGirls) {
+		this.percentageOfGirls = percentageOfGirls;
+	}
+	public double getPercentageOfForeigners() {
+		return percentageOfForeigners;
+	}
+	public void setPercentageOfForeigners(double percentageOfForeigners) {
+		this.percentageOfForeigners = percentageOfForeigners;
+	}
+	
 
 }

fa3545a27596e10b28a2ed71d3f3d326beea9abe
!Simon <!Simon@LeChat>
2014-09-13 16:32:13 +0200

    Added basic mainframe elements

--- src/Gui/Window.java
+++ src/Gui/Window.java
@@ -1,12 +1,19 @@
 package Gui;
 
 import java.awt.Container;
+import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
 
 public class Window extends JFrame{
 	
@@ -14,6 +21,8 @@ public class Window extends JFrame{
 	JTabbedPane tabbPane = new JTabbedPane();
 	JPanel gender = new JPanel(null, true);
 	
+	JLabel arrowLeft;
+	JLabel arrowRight;
 	
 	public Window(String title) {
 		super(title);
@@ -24,17 +33,36 @@ public class Window extends JFrame{
 		this.setSize(frameWidth, frameHeight);
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		int x = (d.width - getSize().width) / 2;
-		int y = (d.height - getSize().height) / 2;
-		
+		int y = (d.height - getSize().height) / 2;		
 		setLocation(x, y);
 		this.setResizable(false);
 		cp = getContentPane();
 		cp.setLayout(null);
 		
+		Image arLeft = Toolkit.getDefaultToolkit().getImage(
+		        getClass().getResource("arrowLeft.png"));
+		Image scaledImage = arLeft.getScaledInstance(100, 100, Image.SCALE_SMOOTH);		
+		ImageIcon IconArLeft = new ImageIcon(scaledImage);
+		
+		Image arRight = Toolkit.getDefaultToolkit().getImage(
+		        getClass().getResource("arrowRight.png"));
+		scaledImage = arRight.getScaledInstance(100, 100, Image.SCALE_SMOOTH);		
+		ImageIcon IconArRight = new ImageIcon(scaledImage);
+		
 		// Komponenten 
-		tabbPane.setBounds(0, 200, 590, 400);
+		tabbPane.setBounds(0, 150, 600, 450);
 		tabbPane.add("Geschlächt", gender);
 		cp.add(tabbPane);
+		
+		arrowLeft = new JLabel(IconArLeft);
+		arrowLeft.setBounds(10, 20, 100, 100);
+		arrowLeft.setCursor(new Cursor(HAND_CURSOR));
+		cp.add(arrowLeft);
+		
+		arrowRight = new JLabel(IconArRight);
+		arrowRight.setBounds(frameWidth - 115, 20, 100, 100);
+		arrowRight.setCursor(new Cursor(HAND_CURSOR));
+		cp.add(arrowRight);
 		
 		// Ende Komponenten
 		
@@ -43,9 +71,9 @@ public class Window extends JFrame{
 	
 	public static void main(String[] args){
 		try {
-			
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 		new Window("Test");
 	}
